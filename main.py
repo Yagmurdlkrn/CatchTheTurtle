d621a4f2635d7fc5c54e503b7b6a6964cd5235d8
Yağmur Dalkıran <yagmurdalkiran1997@gmail.com>
2023-12-04 19:38:50 +0300

    turtle added

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ game_screen.bgcolor("white")
 game_screen.title("Catch The Turtle")
 game_screen.setup(width= 500 , height= 500)
 
-
+#turtle
+turtle_instance = turtle.Turtle()
+turtle_instance.shape("turtle")
+turtle_instance.color("green")
+turtle_instance.penup()
+turtle_instance.speed(0)
 
 turtle.mainloop()
